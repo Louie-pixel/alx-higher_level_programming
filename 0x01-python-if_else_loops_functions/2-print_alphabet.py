9adaa3feea1c1ddc39412a285342f673e2d075ab
Louie_pixel <louiewilli478@gmail.com>
2024-01-02 12:33:41 -0800

    123

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# 10-add.py
+# 2-print_alphabet.py
 # Brennan D Baraban <375@holbertonschool.com>
 
-def add(a, b):
-    """Return the addition of a and b."""
-    return (a + b)
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
