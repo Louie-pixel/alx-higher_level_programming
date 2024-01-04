ea210877880ef3cd71d48fbae3fb2a40bd75eb02
Louie_pixel <louiewilli478@gmail.com>
2024-01-04 10:47:25 -0800

    12

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
-from add_0 import add  # Import the specific function 'add' from the file 'add_0.py'
-
-a = 1  # Assign values to variables a and b in separate lines
-b = 2
-
-result = add(a, b)  # Call the imported 'add' function with a and b as arguments
-
-print(f"{a} + {b} = {result}")  # Print the formatted string with the result
-
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
