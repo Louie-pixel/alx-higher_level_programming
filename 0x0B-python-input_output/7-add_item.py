77083370c30694da169d5bb75bcfbd6106629547
Louie-pixel <louiewilli478@gmail.com>
2024-02-06 02:17:00 -0800

    s

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+Adds all arguments to a Python list and then saves them to a file.
+"""
+
 import sys
+import os.path
+from json import dump, load
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    file_name = "add_item.json"
+    
+    # Check if file exists
+    if os.path.exists(file_name):
+        # Load existing data from file
+        with open(file_name, "r") as f:
+            data = load(f)
+    else:
+        data = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    # Append command line arguments to the data list
+    data.extend(sys.argv[1:])
+
+    # Save the updated list to the file
+    with open(file_name, "w") as f:
+        dump(data, f)
